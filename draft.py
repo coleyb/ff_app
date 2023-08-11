76f914d29896321a1fa130e0b26902f60172c575
Coley Brown <colebrow.cb@gmail.com>
2023-08-10 20:29:08 -0600

    Refactor

--- draft.py
+++ draft.py
@@ -18,34 +18,32 @@ data = response.json()
 
 csv_path = "./fantasy_players.csv"
 
-# Open a CSV file for writing
-with open(csv_path, 'w', newline='') as csvfile:
-    # Add 'is_drafted' to the list of fieldnames
-    fieldnames = list(data[0].keys()) + ['is_drafted']
-    
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    for row in data:
-        # Set the 'is_drafted' value to 0 for each row
-        row['is_drafted'] = 0
-        writer.writerow(row)
-
-
 @st.cache_resource
-def load_fantasy_data(csv_path):
-    """Load the fantasy players data from a CSV."""
-    # Step 1: Read the CSV into a pandas DataFrame
-    df = pd.read_csv(csv_path)
+def load_fantasy_data():
+    """Load the fantasy players data from a URL endpoint, add required columns, and save to CSV."""
+    # Fetch the JSON data
+    response = requests.get(url)
+    data = response.json()
 
-    # Step 2: Add a new column to the DataFrame
+    # Convert JSON to DataFrame
+    df = pd.DataFrame(data)
+    
+    # Add required columns
     df['is_drafted'] = 0
     df['draft_order'] = ''
+    df['my_team'] = False
+    
+    # Sort the DataFrame
     df = df.sort_values(by='ProjectedFantasyPoints', ascending=False)
 
-    # Step 3: Write the DataFrame back to the CSV
+    # Save to CSV
     df.to_csv(csv_path, index=False)
-    return pd.read_csv(csv_path)
+    
+    return df
+
+# Use the function to load the data
+fantasy_data = load_fantasy_data()
+
 
 def get_next_best_available(data, position):
     """Get the next best available player based on the is_drafted column and specified position."""
@@ -62,16 +60,84 @@ def get_next_best_available(data, position):
     # Return the top available player
     return sorted_players.iloc[0]
 
-def set_draft_status(df, player_name, is_drafted=True):
-    """Set the draft status of a player."""
-    player_index = df[df['Name'] == player_name].index[0]
-    df.at[player_index, 'is_drafted'] = is_drafted
+def set_draft_status(df, player_name, drafted=True, to_my_team=False):
+    """
+    Function to set a player's draft status in the DataFrame.
+    If drafted, it will also set the draft order.
+    """
+    idx = df[df['Name'] == player_name].index[0]
     
-    if is_drafted:
-        # If the player is being drafted, set their draft order
+    # Set the draft status
+    df.at[idx, 'is_drafted'] = drafted
+
+    # If the player is drafted, set the draft order
+    if drafted:
         max_order = df['draft_order'].max()
-        next_order = 1 if pd.isna(max_order) else max_order + 1
-        df.at[player_index, 'draft_order'] = next_order
+        if pd.isna(max_order) or max_order == '':
+            next_order = 1
+        else:
+            next_order = int(max_order) + 1
+        df.at[idx, 'draft_order'] = next_order
+
+    # If the player is drafted to "my team", set the 'my_team' flag
+    if to_my_team:
+        df.at[idx, 'my_team'] = True
+
+
+def suggest_pick(df, my_draft_position, num_users):
+    # If we have a current suggested player and he's still undrafted, suggest him again
+    if 'suggested_player_name' in st.session_state and st.session_state.suggested_player_name in df['Name'].values and not df[df['Name'] == st.session_state.suggested_player_name].iloc[0]['is_drafted']:
+        return df[df['Name'] == st.session_state.suggested_player_name].iloc[0]
+
+    # Set positional constraints
+    roster_constraints = {
+        'QB': 1,
+        'WR': 2,
+        'RB': 2,
+        'TE': 1,
+        'FLEX': 1,
+        'DST': 1,
+        'K': 1
+    }
+
+    # Check current composition of your team
+    my_team = df[df['my_team'] == True]
+    for position in ['QB', 'WR', 'RB', 'TE', 'DST', 'K']:
+        roster_constraints[position] -= len(my_team[my_team['Position'] == position])
+
+    # If Flex hasn't been filled, check for best available between WR, RB, TE
+    if roster_constraints['FLEX'] > 0:
+        potential_flex = my_team[(my_team['Position'] == 'WR') | 
+                                (my_team['Position'] == 'RB') | 
+                                (my_team['Position'] == 'TE')]
+        if len(potential_flex) < 3:  # 2 WRs + 1 RB or 2 RBs + 1 WR would fill the Flex
+            roster_constraints['TE'] -= len(potential_flex)
+
+    total_drafted_players = df[df['is_drafted'] == True].shape[0]
+    current_round = (total_drafted_players // num_users) + 1
+
+    # Determine the number of picks before your turn
+    if current_round % 2 == 1:  # Odd rounds
+        picks_before_your_turn = my_draft_position - (total_drafted_players % num_users)
+    else:  # Even rounds
+        picks_before_your_turn = num_users - (total_drafted_players % num_users) + my_draft_position
+
+    # Get undrafted players sorted by projected points
+    undrafted_players = df[df['is_drafted'] == False].sort_values(by='ProjectedFantasyPoints', ascending=False).reset_index(drop=True)
+
+    # Start at the player projected to go around your draft position and check for the first player that fills an immediate need
+    starting_index = my_draft_position - 1
+    for index in range(starting_index, len(undrafted_players)):
+        player = undrafted_players.iloc[index]
+        position = player['Position']
+        if roster_constraints[position] > 0:  # This player fills a need
+            return player
+        if position in ['WR', 'RB', 'TE'] and roster_constraints['FLEX'] > 0:  # This player can be used in a FLEX position
+            return player
+
+    return None  # In case all positions are filled
+
+
 
 # Streamlit
 def main():
@@ -80,17 +146,20 @@ def main():
     num_users = st.number_input('Enter the number of users in the draft:', min_value=2, value=10)
     my_draft_position = st.number_input('Enter your draft position:', min_value=1, max_value=num_users, value=1)
 
+    # Initialize or update the previous_draft_position in session_state
+    if 'previous_draft_position' not in st.session_state:
+        st.session_state.previous_draft_position = my_draft_position
+    else:
+        if st.session_state.previous_draft_position != my_draft_position:
+            if 'suggested_player_name' in st.session_state:
+                del st.session_state.suggested_player_name
+            st.session_state.previous_draft_position = my_draft_position
+
     # Load data
-    fantasy_data = load_fantasy_data(csv_path)
-
-    # Calculate the current draft round for your team
-    total_drafted_players = fantasy_data[fantasy_data['is_drafted'] == True].shape[0]
-    current_round = math.ceil((total_drafted_players + 1) / num_users)
-
-    # Create an input box for search
-    search_query = st.text_input("Search by player or position:")
+    fantasy_data = load_fantasy_data()
 
     # Filter data based on search query and exclude already drafted players
+    search_query = st.text_input("Search by player or position:")
     filtered_data = fantasy_data[
         (fantasy_data['Name'].str.contains(search_query, case=False) | fantasy_data['Position'].str.contains(search_query, case=False))
         & (fantasy_data['is_drafted'] == False)
@@ -100,6 +169,7 @@ def main():
     top_20_players = filtered_data.sort_values(by='ProjectedFantasyPoints', ascending=False).head(20)
 
     # Display drafted players in a separate table, sorted by draft order
+    fantasy_data['draft_order'] = pd.to_numeric(fantasy_data['draft_order'], errors='coerce')
     drafted_players = fantasy_data[fantasy_data['is_drafted'] == True].sort_values(by='draft_order')
 
     if not drafted_players.empty:
@@ -107,90 +177,31 @@ def main():
         
         # Modify display of drafted players based on 'my_team' flag
         drafted_players['Display'] = drafted_players.apply(
-            lambda row: f"{row['Name']} - {row['Position']}",
+            lambda row: f"{row['Name']} - {row['Position']}{' *' if row['my_team'] else ''}",
             axis=1
         )
         
         # Display only the modified 'Display' column
         st.table(drafted_players['Display'])
 
-
-    # Calculate upcoming draft turns based on your position and the snake draft pattern
-    def calculate_draft_turns(my_position, num_teams, num_rounds=10):
-        turns = []
-        for round_num in range(1, num_rounds + 1):
-            if round_num % 2 == 1:  # Odd rounds
-                turn = (round_num - 1) * num_teams + my_position
-            else:  # Even rounds (snake draft)
-                turn = round_num * num_teams - (my_position - 1)
-            turns.append(turn)
-        return turns
-
-    upcoming_turns = calculate_draft_turns(my_draft_position, num_users)
-
-    def suggest_pick(df, draft_position, num_teams):
-        total_drafted_players = df[df['is_drafted'] == True].shape[0]
-        current_round = (total_drafted_players // num_teams) + 1
-
-        # Determine pick number in the current round
-        if current_round % 2 == 1:  # Odd rounds
-            pick_in_round = total_drafted_players % num_teams + 1
-        else:  # Even rounds
-            pick_in_round = num_teams - (total_drafted_players % num_teams)
-
-        # Number of picks before your turn
-        if current_round % 2 == 1:  # Odd rounds
-            picks_before_your_turn = draft_position - pick_in_round
-        else:  # Even rounds
-            picks_before_your_turn = pick_in_round - draft_position
-
-        undrafted_players = df[df['is_drafted'] == False]
-
-        # If all players have been drafted, return None
-        if undrafted_players.empty:
-            return None
-
-        # Suggest a player
-        suggested_index = min(picks_before_your_turn, len(undrafted_players) - 1)
-        suggested_player = undrafted_players.sort_values(by='ProjectedFantasyPoints', ascending=False).iloc[suggested_index]
-        return suggested_player
-
-
-
-    # Suggest players for each upcoming turn
-    suggestions = {}
-    for turn in upcoming_turns:
-        available_players = fantasy_data[(fantasy_data['is_drafted'] == False)].sort_values(by='ProjectedFantasyPoints', ascending=False)
-        # Check if there are still players available
-        if not available_players.empty:
-            # Suggest the highest projected player who is still available around that turn
-            suggested_player = available_players.iloc[min(turn, len(available_players) - 1)]
-            suggestions[turn] = suggested_player['Name']
-
-    next_suggested_player = suggest_pick(fantasy_data, my_draft_position, num_users)
-
-    
-    # Initialize session state variable for drafted player name
-    if 'drafted_name' not in st.session_state:
-        st.session_state.drafted_name = None
-
     # Get the suggested pick
-    next_suggested_player = suggest_pick(fantasy_data, my_draft_position, num_users)
-    if next_suggested_player is not None:
-        st.markdown(f"**Next Suggested Pick:** {next_suggested_player['Name']} - {next_suggested_player['Position']} - {next_suggested_player['ProjectedFantasyPoints']} points - {next_suggested_player['AverageDraftPositionPPR']} ADP - {next_suggested_player['ByeWeek']} bye", unsafe_allow_html=True)
+    if 'suggested_player_name' not in st.session_state:
+        next_suggested_player = suggest_pick(fantasy_data, my_draft_position, num_users)
+        st.session_state.suggested_player_name = next_suggested_player['Name']
     else:
-        st.write("All players have been drafted.")
-
+        next_suggested_player = fantasy_data[fantasy_data['Name'] == st.session_state.suggested_player_name].iloc[0]
+    
+    st.markdown(f"**Next Suggested Pick:** {next_suggested_player['Name']} - {next_suggested_player['Position']} - {next_suggested_player['ProjectedFantasyPoints']} points - {next_suggested_player['AverageDraftPositionPPR']} ADP - {next_suggested_player['ByeWeek']} bye", unsafe_allow_html=True)
 
     # Display the top 20 filtered results with draft buttons
     for index, row in top_20_players.iterrows():
         col1, col2, col3 = st.columns(3)
 
-        # Check if player is among the suggested players for upcoming turns
+        # Check if player is the suggested player
         player_name = row['Name']
         player_details = f"{player_name} - {row['Position']} - {row['ProjectedFantasyPoints']} points - {row['AverageDraftPositionPPR']} ADP - {row['ByeWeek']} bye"
         
-        if player_name == next_suggested_player['Name']:
+        if player_name == st.session_state.suggested_player_name:
             player_display = f"**<span style='color:red;'>{player_details}</span>**"
             with col1:
                 st.markdown(player_display, unsafe_allow_html=True)
@@ -201,26 +212,24 @@ def main():
         with col2:
             # Add a draft button for each player
             draft_button = st.button(f"Draft {player_name}", key=row['Name'])
-            # When the "Draft" or "Draft to My Team" button is clicked:
+            # When the "Draft" button is clicked:
             if draft_button:
                 set_draft_status(fantasy_data, row['Name'], True)
                 fantasy_data.to_csv(csv_path, index=False)
-
-                # Get the next suggested player
-                next_suggested_player = suggest_pick(fantasy_data, my_draft_position, num_users)
-                if next_suggested_player is not None:
-                    st.write(f"The next suggested pick is: {next_suggested_player['Name']}")
-                else:
-                    st.write("All players have been drafted.")
-
-
-    # Check if a player has been drafted and update the data accordingly
-    if st.session_state.drafted_name:
-        set_draft_status(fantasy_data, st.session_state.drafted_name, True)
-        fantasy_data.to_csv(csv_path, index=False)
-        st.write(f"{st.session_state.drafted_name} has been drafted!")
-        # Reset the drafted name in session state
-        st.session_state.drafted_name = None
+                # Remove the suggested player if he's drafted
+                if row['Name'] == st.session_state.suggested_player_name:
+                    del st.session_state.suggested_player_name
+        
+        with col3:
+            # Add a "Draft to My Team" button for each player
+            my_team_button = st.button(f"Draft {row['Name']} to My Team", key=f"MyTeam_{row['Name']}")
+            if my_team_button:
+                set_draft_status(fantasy_data, row['Name'], True, to_my_team=True)
+                fantasy_data.to_csv(csv_path, index=False)
+                st.write(f"{row['Name']} has been drafted to your team!")
+                # Remove the suggested player if he's drafted
+                if row['Name'] == st.session_state.suggested_player_name:
+                    del st.session_state.suggested_player_name
 
 if __name__ == "__main__":
     main()
